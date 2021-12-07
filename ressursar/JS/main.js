2b1c5b06e16e2b4b43bc8d9cbd3480cc20a17141
Asgeir-droid <asgeir.l.aasheim@hotmail.no>
2021-12-07 11:54:44 +0100

    åååååååååååååååååå

--- ressursar/JS/main.js
+++ ressursar/JS/main.js
@@ -1,5 +1,8 @@
 // JavaScript Document
-function darkMode() {
+function myFunction() {
    var element = document.body;
    element.classList.toggle("dark-mode");
+	
+	var meny = document.ul;
+	meny.classList.toggle("meny-dark-mode")
 }
