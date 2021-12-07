65f094ffc298d202ea6fb17f293bb56930258583
Asgeir-droid <asgeir.l.aasheim@hotmail.no>
2021-12-07 12:12:44 +0100

    å
    Co-Authored-By: e-larlon2 <90448558+e-larlon2@users.noreply.github.com>

--- ressursar/JS/main.js
+++ ressursar/JS/main.js
@@ -3,8 +3,5 @@ function myFunction() {
    var element = document.body;
    element.classList.toggle("dark-mode");
 	
-	var meny = document.getElementById("menyDrakMode");
-	document.getElementById("menyDarkMode").style.color = "white";
-	document.getElementById("menyDarkMode").style.backgroundColor = "black";
 
 }
