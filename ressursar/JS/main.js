84d60f95a29410d2d9ac56d4c3855b4aa7df347f
steffen-com <e-steaga@iskule.no>
2022-02-15 12:33:06 +0100

    stor bokstav

--- ressursar/JS/main.js
+++ ressursar/JS/main.js
@@ -10,12 +10,13 @@ function validate() {
    if (document.form1.fullName.value.length < 1) {
      alert("skriv inn navne ditt.");
      return false;
+     
    }
   if (document.form1.email.value.length < 7) {
     alert("skriv inn E-mailen din.");
     return false;
   }
-  if (document.form1.comments.value.length < 3) {
+  if (document.form1.commentar.value.length < 3) {
     alert("skriv inn tilbakemelding.");
     return false;
   }
