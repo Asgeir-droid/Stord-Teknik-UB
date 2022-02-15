5f2cd3c74fb07cd6ee1f68b334425a3d3802255c
steffen-com <e-steaga@iskule.no>
2022-02-15 11:15:31 +0100

    a

--- ressursar/JS/main.js
+++ ressursar/JS/main.js
@@ -6,6 +6,18 @@ function myFunction() {
 
 }
 
-function tilbake() {
-    window.location.href="";
+function validate() {
+   if (document.form1.fullName.value.length < 1) {
+     alert("skriv inn navne.");
+     return false;
+   }
+  if (document.form1.email.value.length < 7) {
+    alert("skriv inn E-mail.");
+    return false;
+  }
+  if (document.form1.comments.value.length < 3) {
+    alert("skriv inn tilbakemelding.");
+    return false;
+  }
+  return true;
 }
